8ac5966302d9c7b71588b02b535eb93adf4dae0b
Kukuh <kukuhsain@gmail.com>
2016-07-13 17:17:43 +0700

    Fix bug of not showing list by changing the text color and clean up some code

--- app/src/main/java/com/kukuhsain/kukuh/learnandroid/todolist/RealmBasedToDoListActivity.java
+++ app/src/main/java/com/kukuhsain/kukuh/learnandroid/todolist/RealmBasedToDoListActivity.java
@@ -2,7 +2,6 @@ package com.kukuhsain.kukuh.learnandroid.todolist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -18,7 +17,6 @@ public class RealmBasedToDoListActivity extends AppCompatActivity {
     private String TAG = "RealmBasedToDoListActivity";
     private ListView toDoListView;
     private ListAdapter listAdapter;
-    private List<String> toDoItems = new ArrayList<String>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,11 +24,24 @@ public class RealmBasedToDoListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_realm_based_todolist);
 
         toDoListView = (ListView) findViewById(R.id.todolist_view);
-        toDoItems.add("satu");
-        toDoItems.add("dua");
-        toDoItems.add("tiga");
-        Log.d(TAG, "list size: "+toDoItems.size());
-        listAdapter = new ToDoListAdapter(toDoItems, getApplicationContext());
+
+        String[] toDos = setupToDoList();
+
+        listAdapter = new ToDoListAdapter(getApplicationContext(), toDos);
         toDoListView.setAdapter(listAdapter);
+    }
+
+    private String[] setupToDoList() {
+        List<String> toDoItems = new ArrayList<String>();
+        toDoItems.add("One");
+        toDoItems.add("Two");
+        toDoItems.add("Three");
+        toDoItems.add("Four");
+        toDoItems.add("Five");
+
+        String[] toDoArray = new String[toDoItems.size()];
+        toDoItems.toArray(toDoArray);
+
+        return toDoArray;
     }
 }
--- app/src/main/java/com/kukuhsain/kukuh/learnandroid/todolist/ToDoListAdapter.java
+++ app/src/main/java/com/kukuhsain/kukuh/learnandroid/todolist/ToDoListAdapter.java
@@ -5,59 +5,31 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.kukuhsain.kukuh.learnandroid.R;
 
-import java.util.List;
-
 /**
  * Created by kukuh on 29/06/16.
  */
-public class ToDoListAdapter extends BaseAdapter {
+public class ToDoListAdapter extends ArrayAdapter<String> {
     private String TAG = "ToDoListAdapter";
-    private List<String> toDoItems;
-    private LayoutInflater inflater;
 
-    public ToDoListAdapter(List<String> toDoItems, Context context) {
-        super();
-        this.toDoItems = toDoItems;
-        this.inflater = LayoutInflater.from(context);
-    }
-
-    @Override
-    public int getCount() {
-        return toDoItems.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return toDoItems.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
+    public ToDoListAdapter(Context context, String[] toDos) {
+        super(context, R.layout.item_todolist, toDos);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Holder holder = new Holder();
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View itemView = inflater.inflate(R.layout.item_todolist, parent, false);
 
-        convertView = inflater.inflate(R.layout.item_todolist, parent, false);
-        holder.itemTitleView = (TextView) convertView.findViewById(R.id.item_title);
-        holder.itemDescriptionView = (TextView) convertView.findViewById(R.id.item_description);
+        Log.d(TAG, getItem(position));
 
-        convertView.setTag(holder);
+        TextView toDoItemText = (TextView) itemView.findViewById(R.id.todoitem_text);
+        toDoItemText.setText(getItem(position));
 
-        Log.d(TAG, "items: "+toDoItems.get(position));
-        holder.itemTitleView.setText(toDoItems.get(position));
-        return convertView;
-    }
-
-    static class Holder {
-        TextView itemTitleView;
-        TextView itemDescriptionView;
+        return itemView;
     }
 }
