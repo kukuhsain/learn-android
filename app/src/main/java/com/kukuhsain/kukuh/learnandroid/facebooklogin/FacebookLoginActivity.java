8a1e6b61579de3a70ba449eafd977761b13b4a20
Kukuh <kukuhsain@gmail.com>
2016-07-15 17:23:12 +0700

    Add callback handler for facebook login integration

--- app/src/main/java/com/kukuhsain/kukuh/learnandroid/facebooklogin/FacebookLoginActivity.java
+++ app/src/main/java/com/kukuhsain/kukuh/learnandroid/facebooklogin/FacebookLoginActivity.java
@@ -1,16 +1,26 @@
 package com.kukuhsain.kukuh.learnandroid.facebooklogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.kukuhsain.kukuh.learnandroid.R;
 
 /**
  * Created by kukuh on 14/07/16.
  */
 public class FacebookLoginActivity extends AppCompatActivity {
+    private final String TAG = "FacebookLoginActivity";
+    private CallbackManager callbackManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +29,34 @@ public class FacebookLoginActivity extends AppCompatActivity {
         AppEventsLogger.activateApp(getApplication());
 
         setContentView(R.layout.activity_facebook_login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "fb login success");
+                Log.d(TAG, "fb login result: "+loginResult.toString());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "fb login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "fb login error: "+error.toString());
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
