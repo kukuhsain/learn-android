07761789307f9bd86681f4e18e8e61b2c679cbb9
Kukuh <kukuhsain@gmail.com>
2016-08-18 15:43:12 +0700

    Unfinished code for google place api implementation

--- app/src/main/java/com/kukuhsain/kukuh/learnandroid/googleplaceapi/GooglePlaceAutocomplete.java
+++ app/src/main/java/com/kukuhsain/kukuh/learnandroid/googleplaceapi/GooglePlaceAutocomplete.java
@@ -1,23 +1,36 @@
 package com.kukuhsain.kukuh.learnandroid.googleplaceapi;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 import com.kukuhsain.kukuh.learnandroid.R;
 
 /**
  * Created by kukuh on 02/08/16.
  */
-public class GooglePlaceAutocomplete extends AppCompatActivity {
+public class GooglePlaceAutocomplete extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener {
     private final String TAG = "GooglePlaceAutocomplete";
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_place_autocomplete);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
 
         Button btnAutocompleteSearch = (Button) findViewById(R.id.btn_autocomplete_search);
         btnAutocompleteSearch.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +39,10 @@ public class GooglePlaceAutocomplete extends AppCompatActivity {
                 Log.d(TAG, "button pressed.......");
             }
         });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "connection failed: "+connectionResult.toString());
     }
 }
